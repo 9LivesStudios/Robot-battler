9fb373385d4bafac7e92f8c821588e9fd30134a5
Legosoldat <alfred.limhamn@live.se>
2014-08-04 23:07:40 +0200

    Created grass and stone tiles

--- Rpg/Rpg/src/game/Game.java
+++ Rpg/Rpg/src/game/Game.java
@@ -4,10 +4,10 @@ import game.gfx.Colours;
 import game.gfx.Font;
 import game.gfx.Screen;
 import game.gfx.SpriteSheet;
+import game.level.Level;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
@@ -16,149 +16,163 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final int WIDTH = 160;
-	public static final int HEIGHT = WIDTH/12*9;
+	public static final int HEIGHT = WIDTH / 12 * 9;
 	public static final int SCALE = 3;
 	public static final String NAME = "Game";
-	
+
 	private JFrame frame;
-	
+
 	public boolean running = false;
 	public int tickCount = 0;
-	
-	private BufferedImage image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
-	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-	private int[] colours = new int[6*6*6];
-	
+
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT,
+			BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer())
+			.getData();
+	private int[] colours = new int[6 * 6 * 6];
+
 	private Screen screen;
 	public InputHandler input;
-	
+	public Level level;
+
 	public Game() {
-		setMinimumSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-		setMaximumSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-		setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-		
+		setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
 		frame = new JFrame(NAME);
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
-		
-		frame.add(this,BorderLayout.CENTER);
+
+		frame.add(this, BorderLayout.CENTER);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-	
+
 	}
-	
+
 	public void init() {
 		int index = 0;
-		for (int r = 0; r < 6;r++) {
-			for (int g = 0; g < 6;g++) {
-				for (int b = 0; b < 6;b++) {
-					int rr = (r * 255/5);
-					int gg = (g * 255/5);
-					int bb = (b * 255/5);
-					
+		for (int r = 0; r < 6; r++) {
+			for (int g = 0; g < 6; g++) {
+				for (int b = 0; b < 6; b++) {
+					int rr = (r * 255 / 5);
+					int gg = (g * 255 / 5);
+					int bb = (b * 255 / 5);
+
 					colours[index++] = rr << 16 | gg << 8 | bb;
 				}
 			}
 		}
-		
-		screen = new Screen(WIDTH,HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+
+		screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
 		input = new InputHandler(this);
+		level = new Level(64, 64);
 	}
-	
+
 	public synchronized void start() {
 		running = true;
 		new Thread(this).start();
 	}
-	
+
 	public synchronized void stop() {
-			running = false;
-		}
-	
+		running = false;
+	}
+
 	public void run() {
 		long lastTime = System.nanoTime();
-		double nsPerTick = 1_000_000_000D/60D;
-		
+		double nsPerTick = 1_000_000_000D / 60D;
+
 		int ticks = 0;
 		int frames = 0;
-		
+
 		long lastTimer = System.currentTimeMillis();
 		double delta = 0;
-		
+
 		init();
-		
+
 		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / nsPerTick;
 			lastTime = now;
 			boolean shouldRender = true;
-			
-			while(delta >= 1) {
+
+			while (delta >= 1) {
 				ticks++;
 				tick();
 				delta -= 1;
 				shouldRender = true;
 			}
-			
+
 			try {
 				Thread.sleep(2);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			if (shouldRender) {
-			frames++;
-			render();
+				frames++;
+				render();
 			}
 
 			if (System.currentTimeMillis() - lastTimer > 1000) {
 				lastTimer += 1000;
-				System.out.println(ticks+" ticks, "+frames+" frames");
+				System.out.println(ticks + " ticks, " + frames + " frames");
 				frames = 0;
 				ticks = 0;
 			}
 		}
 	}
-	
+
+	private int x = 0, y = 0;
+
 	public void tick() {
 		tickCount++;
-		
-		if (input.up.isPressed()) screen.yOffset--;
-		if (input.down.isPressed()) screen.yOffset++;
-		if (input.left.isPressed()) screen.xOffset--;
-		if (input.right.isPressed()) screen.xOffset++;
-		
+
+		if (input.up.isPressed())
+			y--;
+		if (input.down.isPressed())
+			y++;
+		if (input.left.isPressed())
+			x--;
+		if (input.right.isPressed())
+			x++;
+
+		level.tick();
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null){
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
-		
-		for (int y = 0; y < 32; y++) {
-			for (int x = 0; x < 32; x++) {
-				boolean flipX = x % 2 == 0;
-				boolean flipY = y % 2 == 0;
-				screen.render(x << 3, y << 3, 0, Colours.get(555, 505, 055, 550), flipX, flipY);
+
+		int xOffset = x - screen.width / 2;
+		int yOffset = y - screen.height / 2;
+
+		level.renderTiles(screen, xOffset, yOffset);
+
+		for (int x = 0; x < level.width; x++) {
+			int colour = Colours.get(-1, -1, -1, 000);
+			if (x % 10 == 0 && x != 0) {
+				colour = Colours.get(-1, -1, -1, 500);
 			}
+			Font.render((x % 10) + "", screen, 0 + (x * 8), 0, colour);
 		}
-		
-		Font.render("Robot-battler!", screen, 0, 0, Colours.get(000, -1, -1, 555));
-		
+
 		for (int y = 0; y < screen.height; y++) {
 			for (int x = 0; x < screen.width; x++) {
 				int colourCode = screen.pixels[x + y * screen.width];
-				if (colourCode < 255) pixels[x + y * WIDTH] = colours[colourCode];
+				if (colourCode < 255)
+					pixels[x + y * WIDTH] = colours[colourCode];
 			}
 		}
 		Graphics g = bs.getDrawGraphics();
@@ -167,8 +181,8 @@ public class Game extends Canvas implements Runnable{
 		g.dispose();
 		bs.show();
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		new Game().start();
 	}
 }
